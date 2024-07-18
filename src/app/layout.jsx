c38191ab2735f0a451623a1c80fe8406c8b3f2e0
Nirav Khant <29600493+niravkhant@users.noreply.github.com>
2024-07-19 00:54:26 +0530

    chore: Update Tailwind CSS version and remove unused PostCSS configuration

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "@/styles/globals.css";
+import "../styles/globals.css";
 import Sidebar from "@/Components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
